7161d6f79226d98435589cea317dbb56db2ab1ca
gabrielfst30 <gabrielfst30@gmail.com>
2025-11-08 02:03:14 -0300

    fix: corrigindo importacoes

--- test/unit/MoodNftTest.t.sol
+++ test/unit/MoodNftTest.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 import {Test} from "forge-std/Test.sol";
-import {MoodNft} from "../src/MoodNft.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
 import {console} from "forge-std/console.sol";
 
 contract MoodNftTest is Test {
